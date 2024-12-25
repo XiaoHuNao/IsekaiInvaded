44964176b6fa9ba1552aa2b187c5b5c1fb076c13
XiaoHuNao <shiwenbin2250@gmail.com>
2024-12-25 23:40:38 +0800

    create

---
+++ src/main/java/com/xiaohunao/isekai_invaded/IsekaiInvaded.java
@@ -0,0 +1,42 @@
+package com.xiaohunao.isekai_invaded;
+
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(IsekaiInvaded.MODID)
+public class IsekaiInvaded {
+    public static final String MODID = "isekai_invaded";
+
+    public IsekaiInvaded(IEventBus modEventBus, ModContainer modContainer) {
+//        NeoForge.EVENT_BUS.register(this);
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    }
+
+    public static String asDescriptionId(String path) {
+        return MODID + "." + path;
+    }
+
+    public static <T> ResourceKey<T> asResourceKey(ResourceKey<? extends Registry<T>> registryKey, String path) {
+        return ResourceKey.create(registryKey, asResource(path));
+    }
+
+    public static <T> ResourceKey<Registry<T>> asResourceKey(String path) {
+        return ResourceKey.createRegistryKey(asResource(path));
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+    }
+}
---
+++ src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
@@ -0,0 +1,5 @@
+package com.xiaohunao.isekai_invaded.common.init;
+
+public class IIMoments {
+    
+}
