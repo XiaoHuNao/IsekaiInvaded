d39384a0db92082b578545298647b50ba2a68845
XiaoHuNao <shiwenbin2250@gmail.com>
2024-12-27 00:26:26 +0800

    删除影响心态的东西

--- src/main/java/com/xiaohunao/isekai_invaded/common/data/gen/provider/IIRegistryProvider.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/data/gen/provider/IIRegistryProvider.java
@@ -4,11 +4,8 @@ package com.xiaohunao.isekai_invaded.common.data.gen.provider;
 import com.xiaohunao.heaven_destiny_moment.common.init.HDMRegistries;
 import com.xiaohunao.isekai_invaded.IsekaiInvaded;
 import com.xiaohunao.isekai_invaded.common.init.IIMoments;
-import com.xiaohunao.isekai_invaded.common.init.IIStructureSets;
-import com.xiaohunao.isekai_invaded.common.init.IIStructures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 
@@ -17,9 +14,7 @@ import java.util.concurrent.CompletableFuture;
 
 public class IIRegistryProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder REGISTRY_SET_BUILDER = new RegistrySetBuilder()
-            .add(HDMRegistries.Keys.MOMENT, IIMoments::bootstrap)
-            .add(Registries.STRUCTURE, IIStructures::bootstrap)
-            .add(Registries.STRUCTURE_SET, IIStructureSets::bootstrap);
+            .add(HDMRegistries.Keys.MOMENT, IIMoments::bootstrap);
 
     public IIRegistryProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, REGISTRY_SET_BUILDER, Set.of(IsekaiInvaded.MODID, "minecraft"));
