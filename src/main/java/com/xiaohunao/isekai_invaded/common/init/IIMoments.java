26ae81d600e789fadfc5007a186cf71ba0b03894
XiaoHuNao <shiwenbin2250@gmail.com>
2024-12-28 04:27:10 +0800

    复刻原版进度条

--- src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
@@ -1,23 +1,24 @@
 package com.xiaohunao.isekai_invaded.common.init;
 
+import com.xiaohunao.heaven_destiny_moment.client.gui.bar.render.DefaultBarRenderType;
 import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.WorldUniqueMomentCondition;
-import com.xiaohunao.heaven_destiny_moment.common.init.HDMBarRenderTypes;
 import com.xiaohunao.heaven_destiny_moment.common.init.HDMRegistries;
 import com.xiaohunao.heaven_destiny_moment.common.moment.Moment;
-import com.xiaohunao.heaven_destiny_moment.common.moment.moment.DefaultMoment;
+import com.xiaohunao.heaven_destiny_moment.common.moment.moment.RaidMoment;
 import com.xiaohunao.isekai_invaded.IsekaiInvaded;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 
 public class IIMoments {
-    public static final ResourceKey<Moment> PIGLIN_LEGION = IsekaiInvaded.asResourceKey(HDMRegistries.Keys.MOMENT, "piglin_legion");
+    public static final ResourceKey<Moment<?>> PIGLIN_LEGION = IsekaiInvaded.asResourceKey(HDMRegistries.Keys.MOMENT, "piglin_legion");
 
-    public static void bootstrap(BootstrapContext<Moment> context) {
-        context.register(PIGLIN_LEGION, new DefaultMoment()
-                        .setBarRenderType(HDMBarRenderTypes.DEFAULT.get()).setMomentData(momentData -> momentData
-                                        .conditionGroup(group -> group
-                                                        .create(false, WorldUniqueMomentCondition.DEFAULT)
-                                        )
+    public static void bootstrap(BootstrapContext<Moment<?>> context) {
+        context.register(PIGLIN_LEGION, new RaidMoment()
+                        .setBarRenderType(new DefaultBarRenderType())
+                        .setMomentData(momentData -> momentData
+                                .conditionGroup(group -> group
+                                                .create(false, WorldUniqueMomentCondition.DEFAULT)
+                                )
                         )
         );
     }
