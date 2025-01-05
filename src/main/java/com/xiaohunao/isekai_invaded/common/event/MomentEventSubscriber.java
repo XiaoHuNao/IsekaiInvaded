60a3de531980816661298f1c04ad9f0807f4c7e5
XiaoHuNao <shiwenbin2250@gmail.com>
2025-01-05 16:03:37 +0800

    raid

---
+++ src/main/java/com/xiaohunao/isekai_invaded/common/event/MomentEventSubscriber.java
@@ -0,0 +1,39 @@
+package com.xiaohunao.isekai_invaded.common.event;
+
+import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstance;
+import com.xiaohunao.isekai_invaded.common.init.IIMoments;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+
+@EventBusSubscriber
+public class MomentEventSubscriber {
+//    @SubscribeEvent
+//    public static void onBlock(BlockEvent.PortalSpawnEvent event) {
+//        LevelAccessor level = event.getLevel();
+//        if (level instanceof ServerLevel serverLevel) {
+//            MomentInstance.create(IIMoments.PIGLIN_LEGION,serverLevel,event.getPos(),null);
+//        }
+//        event.setCanceled(true);
+//    }
+
+    @SubscribeEvent
+    public static void onPlayerInteractRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        Level level = event.getLevel();
+        InteractionHand hand = event.getHand();
+        Player player = event.getEntity();
+        if (level.isClientSide() || hand != InteractionHand.MAIN_HAND) {
+            return;
+        }
+
+        if (level instanceof ServerLevel serverLevel) {
+//            MomentInstance.create(IIMoments.PIGLIN_LEGION,serverLevel,event.getPos(),null);
+        }
+    }
+}
--- src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
@@ -1,13 +1,21 @@
 package com.xiaohunao.isekai_invaded.common.init;
 
+import com.mojang.serialization.MapCodec;
 import com.xiaohunao.heaven_destiny_moment.client.gui.bar.render.DefaultBarRenderType;
 import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.WorldUniqueMomentCondition;
+import com.xiaohunao.heaven_destiny_moment.common.context.entity_info.EntityInfo;
 import com.xiaohunao.heaven_destiny_moment.common.init.HDMRegistries;
 import com.xiaohunao.heaven_destiny_moment.common.moment.Moment;
+import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstance;
 import com.xiaohunao.heaven_destiny_moment.common.moment.moment.RaidMoment;
+import com.xiaohunao.heaven_destiny_moment.common.spawn_algorithm.ISpawnAlgorithm;
+import com.xiaohunao.heaven_destiny_moment.common.spawn_algorithm.OpenAreaSpawnAlgorithm;
 import com.xiaohunao.isekai_invaded.IsekaiInvaded;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.phys.Vec3;
 
 public class IIMoments {
     public static final ResourceKey<Moment<?>> PIGLIN_LEGION = IsekaiInvaded.asResourceKey(HDMRegistries.Keys.MOMENT, "piglin_legion");
@@ -16,6 +24,28 @@ public class IIMoments {
         context.register(PIGLIN_LEGION, new RaidMoment()
                         .setBarRenderType(new DefaultBarRenderType())
                         .setMomentData(momentData -> momentData
+                                .entitySpawnSettings(entitySpawnSettings -> entitySpawnSettings
+                                        .spawnAlgorithm(new OpenAreaSpawnAlgorithm(10,16))
+                                        .entitySpawnList(
+                                                new EntityInfo.Builder(EntityType.PIGLIN)
+                                                    .amount(2)
+                                                    .build()
+                                        )
+                                        .entitySpawnList(
+                                                new EntityInfo.Builder(EntityType.PIGLIN)
+                                                        .amount(2)
+                                                        .weight(10)
+                                                        .build(),
+                                                new EntityInfo.Builder(EntityType.HOGLIN)
+                                                        .amount(2)
+                                                        .weight(10)
+                                                        .build(),
+                                                new EntityInfo.Builder(EntityType.WITHER_SKELETON)
+                                                        .amount(2)
+                                                        .weight(10)
+                                                        .build()
+                                        )
+                                )
                                 .conditionGroup(group -> group
                                                 .create(false, WorldUniqueMomentCondition.DEFAULT)
                                 )
