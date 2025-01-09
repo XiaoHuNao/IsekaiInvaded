db1cb89b402eef1dce635da1115514d10523fe72
XiaoHuNao <shiwenbin2250@gmail.com>
2025-01-09 15:08:12 +0800

    准备旧日军团

--- src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
@@ -26,8 +26,15 @@ import net.minecraft.world.item.Items;
 
 public class IIMoments {
     public static final ResourceKey<Moment<?>> PIGLIN_LEGION = IsekaiInvaded.asResourceKey(HDMRegistries.Keys.MOMENT, "piglin_legion");
+    public static final ResourceKey<Moment<?>> OLD_ONES_ARMY = IsekaiInvaded.asResourceKey(HDMRegistries.Keys.MOMENT, "old_ones_army ");
 
     public static void bootstrap(BootstrapContext<Moment<?>> context) {
+        context.register(OLD_ONES_ARMY,new RaidMoment());
+
+
+
+
+
         context.register(PIGLIN_LEGION, new RaidMoment()
                 .setBarRenderType(new DefaultBarRenderType())
                 .setMomentData(momentData -> momentData
