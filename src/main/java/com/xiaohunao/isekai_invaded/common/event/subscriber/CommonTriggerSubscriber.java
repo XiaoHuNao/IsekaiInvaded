ab138736716c013049e4ec5b9057cac6d43b6376
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-07-16 19:13:44 +0800

    创建时刻实例改成Builder模式

--- src/main/java/com/xiaohunao/isekai_invaded/common/event/subscriber/CommonTriggerSubscriber.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/event/subscriber/CommonTriggerSubscriber.java
@@ -1,6 +1,7 @@
 package com.xiaohunao.isekai_invaded.common.event.subscriber;
 
 
+import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstanceBuilder;
 import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstanceManager;
 import com.xiaohunao.heaven_destiny_moment.common.moment.moment.instance.RaidInstance;
 import com.xiaohunao.isekai_invaded.common.event.NetherPortalSpawnPigLinEvent;
@@ -27,8 +28,7 @@ public class CommonTriggerSubscriber {
         event.setCanceled(true);
         ServerLevel level = event.getLevel();
         BlockPos pos = event.getPos();
-        MomentInstanceManager momentInstanceManager = MomentInstanceManager.of(level);
-        momentInstanceManager.createMomentInstance(IIMoments.PIGLIN_LEGION.get(),pos,null, momentInstance -> {
+        MomentInstanceBuilder.builder(level,IIMoments.PIGLIN_LEGION.get()).pos(pos).modifier(momentInstance -> {
             if (momentInstance instanceof RaidInstance raidInstance){
                 raidInstance.setOriginalPos(Vec3.atLowerCornerOf(pos));
                 Structure structure = level.registryAccess().registryOrThrow(Registries.STRUCTURE).get(IIStructures.PIGLIN_LEGION);
