a2d07f861fafec2b6b1aa910a9ac1f269a99c5f5
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-08-30 16:50:52 +0800

    更换IMoment标记

--- src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
+++ src/main/java/com/xiaohunao/isekai_invaded/common/init/IIMoments.java
@@ -2,14 +2,16 @@ package com.xiaohunao.isekai_invaded.common.init;
 
 import com.xiaohunao.heaven_destiny_moment.api.MomentManager;
 import com.xiaohunao.heaven_destiny_moment.common.init.HDMRegistries;
+import com.xiaohunao.heaven_destiny_moment.common.moment.IMoment;
 import com.xiaohunao.heaven_destiny_moment.common.moment.Moment;
+import com.xiaohunao.heaven_destiny_moment.common.moment.moment.RaidMoment;
 import com.xiaohunao.isekai_invaded.IsekaiInvaded;
-import com.xiaohunao.xhn_lib.api.register.FlexibleHolder;
-import com.xiaohunao.xhn_lib.api.register.FlexibleRegister;
+import com.xiaohunao.xhn_lib.api.register.holder.FlexibleHolder;
+import com.xiaohunao.xhn_lib.api.register.register.FlexibleRegister;
 
 public class IIMoments {
-    public static final FlexibleRegister<Moment> MOMENT = FlexibleRegister.create(HDMRegistries.MOMENT,IsekaiInvaded.MODID, MomentManager.getInstance());
+    public static final FlexibleRegister<IMoment> MOMENT = FlexibleRegister.create(HDMRegistries.MOMENT,IsekaiInvaded.MODID, MomentManager.getInstance());
 
-    public static final FlexibleHolder<Moment,?> PIGLIN_LEGION = MOMENT.registerDynamic("piglin_legion");
+    public static final FlexibleHolder<IMoment, RaidMoment> PIGLIN_LEGION = MOMENT.registerDynamic("piglin_legion");
 
 }
